5b5d4a8637c764c15f6b0d698f9d56d01de66072
sovulnerableinu <inhoon05@naver.com>
2026-01-20 15:06:58 +0900

    Update 1620 나는야 포켓몬 마스터 이다솜 .cpp

--- Silver/1620 나는야 포켓몬 마스터 이다솜 .cpp
+++ Silver/1620 나는야 포켓몬 마스터 이다솜 .cpp
@@ -15,18 +15,24 @@ bool isNumber(const string& s)
 }
 int main()
 {
+	// 새로 추가
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
 	int n, m;
 	cin >> n >> m;
 
-	map<int, string> int_key;
-	map<string, int> string_key;
+	//map<int, string> int_key;
+	vector<string> int_v;
+	map<string, int> string_map;
 
 	string name;
 	for (int i = 0; i < n; i++)
 	{
 		cin >> name;
-		int_key.insert({ i + 1, name });
-		string_key.insert({ name,i + 1 });
+		//int_key.insert({ i + 1, name });
+		int_v.push_back(name);
+		string_map.insert({ name,i + 1 });
 	}
 
 	int pnumber;
@@ -37,9 +43,18 @@ int main()
 	{
 		cin >> problem;
 
-		if (isNumber(problem))
+		// problem이 숫자면
+		if (isdigit(problem[0]))
 		{
-			auto it = int_key.find(int(stoi(pname)));
+			//auto it = int_key.find(int(stoi(problem))); // string을 int로 변경해서 검색
+			//cout << it->second;
+			cout << int_v[stoi(problem) - 1];
 		}
+		else // problem이 문자열이면
+		{
+			auto it = string_map.find((problem)); // string을 그대로 key로 검색
+			cout << it->second;
+		}
+		cout << '\n';
 	}
 }
